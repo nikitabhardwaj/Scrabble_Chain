2a01693677acbac56b73de594734dc389a62d57b
Sabyasachi <63522824+sabyasachichoudhuri@users.noreply.github.com>
2020-04-26 04:51:17 +0530

    Wallet
    Nodes issue to be solved

---
+++ Wallet/wallet_blockchain.py
@@ -0,0 +1,51 @@
+from flask import Flask, jsonify, render_template
+from time import time
+import Crypto
+import Crypto.Random
+from Crypto.PublicKey import RSA
+
+
+class Blockchain:
+
+    def __init__(self):
+        self.transactions = []
+        self.chain = []
+        # Create the genesis block
+        self.create_block(0, '00')
+
+    def create_block(self, nonce, previous_hash):
+        """
+        Add a block of transactions to the blockchain
+        """
+        block = {'block_number': len(self.chain) + 1,
+                 'timestamp': time(),
+                 'transactions': self.transactions,
+                 'nonce': nonce,
+                 'previous_hash': previous_hash}
+
+        # Reset the current list of transactions
+        self.transactions = []
+        self.chain.append(block)
+
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+# Instantiate the Node
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('/wallet_blockchain.html')
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5001, type=int, help="port to listen to")
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='127.0.0.1', port=port, debug=True)
